adeecab779322734769ac23a858ff61095e08886
eglpass <3493209254@qq.com>
2025-03-08 17:07:47 +0800

    day2-提交

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -14,7 +14,7 @@ public interface EmployeeMapper {
 
     /**
      * 根据用户名查询员工
-     * @param username
+     * @param username要让他
      * @return
      */
     @Select("select * from employee where username = #{username}")
